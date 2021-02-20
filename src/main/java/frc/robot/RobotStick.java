ad670f2b62b9b63a4e7dd8279dfa111b63c84d5a
Sean <2102062@geneva304.org>
2021-02-20 12:31:14 -0600

    Fixed RobotStick bug

--- src/main/java/frc/robot/RobotStick.java
+++ src/main/java/frc/robot/RobotStick.java
@@ -6,19 +6,19 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotStick extends Joystick {
-    private final int MAX_BUTTONS = 12;
+    private final int HIGHEST_BUTTON = 12;
     private JoystickButton[] buttons;
 
     public RobotStick(int port) {
         super(port);
-        buttons = new JoystickButton[MAX_BUTTONS];
-        for(int i = 0; i < MAX_BUTTONS; i++) {
+        buttons = new JoystickButton[HIGHEST_BUTTON+1];
+        for(int i = 0; i <= HIGHEST_BUTTON; i++) {
             buttons[i] = new JoystickButton(this, i);
         }
     }
     
     public JoystickButton getButton(int button) {
-        if(button >= MAX_BUTTONS| button < 0) {
+        if(button > HIGHEST_BUTTON || button < 0) {
             throw new RangeException(RangeException.BAD_BOUNDARYPOINTS_ERR, "Button not in range on joystick");
         }
         return buttons[button];
